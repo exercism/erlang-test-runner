7f03720ed43e23a8690ac6b22bbd7ab2aba2fccf
Norbert Melzer <timmelzer@gmail.com>
2021-02-22 23:07:18 +0100

    run the test and grade it

--- src/etr_runner.erl
+++ src/etr_runner.erl
@@ -9,7 +9,13 @@ run(Module) ->
      || {Fun, _} <- TestModule:module_info(exports),
         match == re:run(atom_to_binary(Fun, utf8), "_test_?$", [{capture, none}])
     ],
-    [run_case(TestModule, Fun, Gen) || {Fun, Gen} <- TestFuns].
+    Results = [run_case(TestModule, Fun, Gen) || {Fun, Gen} <- TestFuns],
+    Tests = [grade_test(Result) || Result <- Results],
+    #{
+        version => 2,
+        status => get_status(Tests),
+        tests => Tests
+    }.
 
 get_test_module_from(Module) when is_atom(Module) ->
     get_test_module_from(atom_to_binary(Module, utf8));
@@ -28,8 +34,50 @@ run_case(Module, Gen, true) ->
     {list_to_binary(Description), run_case(Fun)}.
 
 run_case(Fun) ->
-    try Fun() of
-        Result -> Result
-    catch
-        Error -> Error
+    {Self, Ref} = {self(), make_ref()},
+    {Pid, MonRef} = erlang:spawn_monitor(fun() -> Self ! {Ref, Fun()} end),
+    receive
+        {Ref, Anything} ->
+            {pass, sweep_inbox(Ref, MonRef, Anything)};
+        {'DOWN', MonRef, process, Pid, {Exception, _}} ->
+            {fail, Exception}
     end.
+
+sweep_inbox(Ref, MRef, Result) ->
+    receive
+        {Ref, _} -> sweep_inbox(Ref, MRef, Result);
+        {'DOWN', MRef, process, _, _} -> sweep_inbox(Ref, MRef, Result)
+    after 0 -> Result
+    end.
+
+grade_test({Name, {pass, ok}}) ->
+    #{
+        name => Name,
+        status => <<"pass">>
+    };
+grade_test({Name, {fail, Exception}}) ->
+    Message = iolist_to_binary(humanize(Exception)),
+    #{
+        name => Name,
+        status => <<"fail">>,
+        message => Message
+    }.
+
+humanize({assertStringEqual, Info}) ->
+    io_lib:format(
+        "The expression `~s` was expected to return a string or binary "
+        "or nested list of either, that compares equal to ~p, but it returned "
+        "~p instead",
+        [
+            proplists:get_value(expression, Info),
+            proplists:get_value(expected, Info),
+            proplists:get_value(value, Info)
+        ]
+    ).
+
+get_status(Tests) -> get_status(Tests, <<"pass">>).
+
+get_status([], Status) -> Status;
+get_status([#{status := <<"error">>} | _], _) -> <<"error">>;
+get_status([#{status := <<"fail">>} | T], _) -> get_status(T, <<"fail">>);
+get_status([_ | T], Status) -> get_status(T, Status).
