1e9f64f0cb35441126a21edd7f81e1b17c041857
Norbert Melzer <timmelzer@gmail.com>
2021-03-28 00:20:14 +0100

    write JSON to disk

--- src/erlang_test_runner.erl
+++ src/erlang_test_runner.erl
@@ -8,14 +8,22 @@
 %%====================================================================
 
 %% escript Entry point
-main([Exercise, InputDir, _OutputDir]) ->
+main([Exercise, InputDir, OutputDir]) ->
+    file:set_cwd(InputDir),
     {Module, Modules} = etr_compile:compile(InputDir, Exercise),
     code:atomic_load(Modules),
-    eunit:start(),
-    R = eunit:test(Module),
-    io:format("R: ~p~n", [R]),
+    Results = etr_runner:run(Module),
+    io:format("~p~n", [Results]),
+    ResultsJson = jsx:encode(Results, [space, {indent, 2}]),
+    io:format("~s~n", [ResultsJson]),
+    ok = write_result(ResultsJson, OutputDir),
     erlang:halt(0).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+write_result(Content, OutputDir) ->
+    ResultJsonPath = filename:join(OutputDir, "results.json"),
+    {ok, FD} = file:open(ResultJsonPath, [write]),
+    io:format(FD, "~s~n", [Content]).
