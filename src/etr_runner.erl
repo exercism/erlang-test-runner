c7a76b5e419d99803e50a13f5cb914124a87ccc6
Norbert Melzer <nmelzer@nobbz.dev>
2021-11-01 18:12:22 +0100

    fix unknown assert crash (#21)

--- src/etr_runner.erl
+++ src/etr_runner.erl
@@ -109,14 +109,23 @@ humanize({assertMatch, Info}) ->
             proplists:get_value(value, Info)
         ]
     );
+humanize({assertEqual, Info}) ->
+    io_lib:format(
+        "The expression `~p` was expected to return a value that equals the "
+        "value `~p`, though the returned value `~p` is different",
+        [
+            proplists:get_value(expression, Info),
+            proplists:get_value(expected, Info),
+            proplists:get_value(value, Info)
+        ]
+    );
 humanize({Assertion, Info}) ->
     io_lib:format(
         "An unknown assertion occured and failed, please report an issue at "
         "https://github.com/exercism/erlang-test-runner/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc "
         "for the '~p' assertion unless it already exists. Please privide the "
         "following additional info: Exercise, failed test and assertion info: '~p'",
-        Assertion,
-        Info
+        [Assertion, Info]
     ).
 
 get_status(Tests) -> get_status(Tests, <<"pass">>).
