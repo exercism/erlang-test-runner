dfd7b598a061c3369606eb9b5c59cf176689a858
Feynman Liang <feynman.liang@gmail.com>
2023-03-09 09:06:54 -0800

    Fix privide->provide typo

--- src/etr_runner.erl
+++ src/etr_runner.erl
@@ -123,7 +123,7 @@ humanize({Assertion, Info}) ->
     io_lib:format(
         "An unknown assertion occured and failed, please report an issue at "
         "https://github.com/exercism/erlang-test-runner/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc "
-        "for the '~p' assertion unless it already exists. Please privide the "
+        "for the '~p' assertion unless it already exists. Please provide the "
         "following additional info: Exercise, failed test and assertion info: '~p'",
         [Assertion, Info]
     ).
